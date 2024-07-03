53b805d7fed28bd1d9e0c133151302f18715b97b
lawrence910426 <lawrence910426@gmail.com>
2024-07-03 11:46:22 +0800

    feat: update on smart contracts

--- backend/contract.sol
+++ backend/contract.sol
@@ -7,12 +7,6 @@ interface IERC20 {
     function transfer(address _to, uint256 _value) external returns (bool success);
 }
 
-enum Side {
-    Yes,
-    No,
-    Unknown
-}
-
 contract Game {
     IERC20 public usdtToken;
 
@@ -28,7 +22,8 @@ contract Game {
     uint public settlement;
     uint public stake_deadline;
 
-    Side public outcome;
+    bool public outcome_win;
+    bool public outcome_unknown;
 
     constructor(address _usdtTokenAddress, uint _settlement, uint _stake_deadline, uint256 _maximum_stake) {
         usdtToken = IERC20(_usdtTokenAddress);
@@ -40,66 +35,63 @@ contract Game {
         settlement = _settlement;
         Maximum_Stake = _maximum_stake;
 
-        outcome = Side.Unknown;
+        outcome_win = false;
+        outcome_unknown = true;
     }
 
-    function stake(uint256 amount, Side side) external {
-        require(side == Side.Yes || side == Side.No);
+    function stake(uint256 amount, bool is_win) external {
         require(block.timestamp < stake_deadline);
         require(amount > 0 && amount + Yes_Total + No_Total <= Maximum_Stake, "Exceed maximum stake amount");
 
         // Receive the ERC-20 token transfer
         require(usdtToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
 
-        if (side == Side.Yes) {
+        if (is_win) {
             Yes_Total += amount;
             Yes_Traders[msg.sender] += amount;
-        }
-        if (side == Side.No) {
+        } else {
             No_Total += amount;
             No_Traders[msg.sender] += amount;
         }
     }
 
     function claim() external {
-        if (outcome == Side.Yes) {
-            uint256 value = Yes_Traders[msg.sender] / Yes_Total * No_Total + Yes_Traders[msg.sender];
-            Yes_Total -= Yes_Traders[msg.sender];
-            Yes_Traders[msg.sender] = 0;
+        if (!outcome_unknown) {
+            // There is a result for the game.
+            uint256 value = 0;
+            if (outcome_win) {
+                Yes_Traders[msg.sender] * No_Total / Yes_Total + Yes_Traders[msg.sender];
+                Yes_Total -= Yes_Traders[msg.sender];
+                Yes_Traders[msg.sender] = 0;
+            } else {
+                No_Traders[msg.sender] * Yes_Total / No_Total + No_Traders[msg.sender];
+                No_Total -= No_Traders[msg.sender];
+                No_Traders[msg.sender] = 0;
+            }
+
+            // Claim the rewards back with 5% of commision.
             usdtToken.transfer(msg.sender, value * 95 / 100);
             usdtToken.transfer(owner, value * 5 / 100);
-        }
 
-        if (outcome == Side.No) {
-            uint256 value = No_Traders[msg.sender] / No_Total * Yes_Total + No_Traders[msg.sender];
-            No_Total -= No_Traders[msg.sender];
-            No_Traders[msg.sender] = 0;
-            usdtToken.transfer(msg.sender, value * 95 / 100);
-            usdtToken.transfer(owner, value * 5 / 100);
-        }
+        } else {
+            // There is no result for the game yet.
+            if (block.timestamp > settlement) {
+                // Claim their stakes back without any commision.
+                Yes_Total -= Yes_Traders[msg.sender];
+                usdtToken.transfer(msg.sender, Yes_Traders[msg.sender]);
+                Yes_Traders[msg.sender] = 0;
 
-        if (outcome == Side.Unknown && block.timestamp > settlement) {
-            Yes_Total -= Yes_Traders[msg.sender];
-            usdtToken.transfer(msg.sender, Yes_Traders[msg.sender]);
-            Yes_Traders[msg.sender] = 0;
-
-            No_Total -= No_Traders[msg.sender];
-            usdtToken.transfer(msg.sender, No_Traders[msg.sender]);
-            No_Traders[msg.sender] = 0;
+                No_Total -= No_Traders[msg.sender];
+                usdtToken.transfer(msg.sender, No_Traders[msg.sender]);
+                No_Traders[msg.sender] = 0;
+            }
         }
     }
 
-    function set_outcome(Side _outcome) public {
+    function set_outcome(bool is_win) public {
         require(msg.sender == owner);
-        outcome = _outcome;
-    }
-
-    function get_stake_total() public view returns (uint256, uint256) {
-        return (Yes_Total, No_Total);
-    }
-
-    function get_stake() public view returns (uint256, uint256) {
-        return (Yes_Traders[msg.sender], No_Traders[msg.sender]);
+        outcome_win = is_win;
+        outcome_unknown = false;
     }
 
     function can_stake() public view returns (bool) {
@@ -107,7 +99,7 @@ contract Game {
     }
 
     function can_claim() public view returns (bool) {
-        return outcome != Side.Unknown || block.timestamp > settlement;
+        return (!outcome_unknown) || (block.timestamp > settlement && outcome_unknown);
     }
 
     function max_stake() public view returns (uint256) {
