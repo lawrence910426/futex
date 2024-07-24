1c4697897f0f33740ac9f64fb2065f118d128f7f
lawrence910426 <lawrence910426@gmail.com>
2024-07-24 22:24:47 +0800

    feat: use docker to build

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@ rm -r nginx/build
 source ~/.bash_profile
 
 cd frontend
-npm install
-npm run build
+docker build . -t frontend-build
+docker run -v $(pwd)/build:/app/build frontend-build
 cp -r build ../nginx
+cd ..
 
 docker compose build --no-cache
 docker compose up
