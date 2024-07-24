b37e8c49ff246099a4f0695161a1b7393d3d4ae3
lawrence910426 <lawrence910426@gmail.com>
2024-07-25 00:37:39 +0800

    feat: supports cloudflare https offloading

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -r nginx/build
 rm -r frontend/build
 
@@ -8,6 +10,20 @@ docker build . -t frontend-build
 docker run -v $(pwd)/build:/opt/build frontend-build
 cp -r build ../nginx
 cd ..
+
+
+placeholder="\$is_behind_cloudflare"
+
+if [[ "$1" == "--cloudflare" ]]; then
+    replacement="https"
+    echo "Enabled Cloudflare. Updating nginx.conf..."
+else
+    replacement="\$scheme"
+    echo "Enabled Cloudflare. Updating nginx.conf..."
+fi
+
+cp nginx/nginx.conf.template nginx/nginx.conf
+sed -i "" "s/${placeholder}/${replacement}/g" "nginx/nginx.conf"
 
 docker compose build --no-cache
 docker compose up
