f36d8e5d5583419d9e501f0ac66fedcaad8f4b5c
lawrence910426 <lawrence910426@gmail.com>
2024-07-25 00:52:34 +0800

    feat: determine on macOS or Linux

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,15 @@ fi
 
 rm nginx/nginx.conf
 cp nginx/nginx.conf.template nginx/nginx.conf
-awk -v placeholder="${placeholder}" -v replacement="${replacement}" '{gsub(placeholder, replacement); print}' nginx/nginx.conf.template > nginx/nginx.conf
+
+# Determine if the script is running on macOS or Linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS sed command
+    sed -i '' "s/${placeholder}/${replacement}/g" nginx/nginx.conf
+else
+    # Linux sed command
+    sed -i "s/${placeholder}/${replacement}/g" nginx/nginx.conf
+fi
 
 docker compose build --no-cache
 docker compose up
